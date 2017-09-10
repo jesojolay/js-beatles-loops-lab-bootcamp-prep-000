04d845d51b92ec82ce8e1b00f39aba7f53314c47
jasmine Magdi <jesojolay@yahoo.com>
2017-09-10 19:53:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function theBeatlesPlay(musicians, instruments){
     var myarr = [];
     for (var i = 0; i < musicians.length; i++) {
         myarr.push(`${musicians[i]} plays ${instruments[i]}`);
-        
     }
+    return myarr;
 }
