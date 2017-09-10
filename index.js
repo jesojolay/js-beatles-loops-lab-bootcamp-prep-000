2fbde4e9ac16a94d37c56152a82b24a62e0d5ce9
jasmine Magdi <jesojolay@yahoo.com>
2017-09-10 19:58:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function theBeatlesPlay(musicians, instruments){
 function johnLennonFacts(facts){
     var counter = 0;
     while (counter < array.length) {
-        facts[counter]+"!!!";
+        facts[counter]+="!!!";
     }
 }
