4731f667d50f368771b6a4a8c22830d324b29650
jasmine Magdi <jesojolay@yahoo.com>
2017-09-10 20:02:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function theBeatlesPlay(musicians, instruments) {
     }
     return myarr;
 }
-const facts = [
+var facts = [
     "He was the last Beatle to learn to drive",
     "He was never a vegetarian",
     "He was a choir boy and boy scout",
