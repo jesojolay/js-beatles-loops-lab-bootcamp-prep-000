262c4134aff42bc4b15076a362ca0717ebb37e1c
jasmine Magdi <jesojolay@yahoo.com>
2017-09-10 20:00:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ function theBeatlesPlay(musicians, instruments) {
     }
     return myarr;
 }
-
-function johnLennonFacts(facts) {
-    const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
+const facts = [
+"He was the last Beatle to learn to drive",
+"He was never a vegetarian",
+"He was a choir boy and boy scout",
+"He hated the sound of his own voice"
 ];
+function johnLennonFacts(facts) {
+
     var counter = 0;
     while (counter < array.length) {
         facts[counter] += "!!!";
